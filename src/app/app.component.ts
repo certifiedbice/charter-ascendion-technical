97febe011473b5b25491017cc5727b8b13ce419d
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-11-28 14:27:20 -0700

    refactor(Architecture): Updated app.component.ts
    Imports, implements, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// Angular
+import {
+	Component,
+	OnInit,
+	AfterContentInit,
+	AfterContentChecked,
+	AfterViewInit,
+	AfterViewChecked,
+	OnDestroy,
+} from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent
+	implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+{
+	constructor() {}
+	// Properties
 	title = 'technical';
+	// Life cycle hooks
+	ngOnInit(): void {
+		console.log('AppComponent');
+	}
+	ngAfterContentInit(): void {}
+	ngAfterContentChecked(): void {}
+	ngAfterViewInit(): void {}
+	ngAfterViewChecked(): void {}
+	ngOnDestroy(): void {}
 }

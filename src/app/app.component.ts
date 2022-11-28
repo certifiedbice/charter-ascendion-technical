b916e9cf1197f8b296bc2348e57370fa0293de62
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-11-28 16:31:35 -0700

    feat(Architecture): Installed  SASS/SCSS
    Implemented SCSS scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.sass'],
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent
 	implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy

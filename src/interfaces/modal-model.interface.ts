d4a3fcafc597c130dd3ce6d5babab48cc8701dd6
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-11-29 12:10:47 -0700

    feat(Architecture): Created modal interface

---
+++ src/interfaces/modal-model.interface.ts
@@ -0,0 +1,17 @@
+interface ModalModelInterface {
+	/**
+	 * Purpose:
+	 *
+	 * Interface for defining a login form,
+	 * needed for typing/defining parameters of a login form.
+	 */
+	// button: {
+	id: string;
+	classList: Array<string>;
+	title: string;
+	body: string;
+	actions: Array<string>;
+	// };
+}
+
+export default ModalModelInterface;

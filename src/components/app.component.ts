f770d04385d7bcdc88f8f9afddeeb91846362fa0
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-11-29 08:39:23 -0700

    feat(FrontEnd): Updated app.component.ts
    Now using overlay and modal components

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,17 +1,28 @@
 // Angular
 import {
-	Component,
-	OnInit,
-	AfterContentInit,
 	AfterContentChecked,
-	AfterViewInit,
+	AfterContentInit,
 	AfterViewChecked,
+	AfterViewInit,
+	Component,
 	OnDestroy,
+	OnInit,
 } from '@angular/core';
-
+// Models
+import ButtonModel from '../../src/store/app/models/button-model';
 @Component({
 	selector: 'app-root',
-	templateUrl: '../html/app.component.html',
+	template: `
+		<div>
+			<button-atom [button_data]="buttonModel" (toggle)="buttonEmissionHandler()"></button-atom>
+			<overlay-atom [overlay]="overlay">
+				<!-- Component ideally placed as the :nth-last-child(2) in the Body element, z-index assists here -->
+			</overlay-atom>
+			<modal-molecule *ngIf="modal">
+				<!-- Component ideally placed as the :nth-last-child(2) in the Body element, z-index assists here -->
+			</modal-molecule>
+		</div>
+	`,
 })
 export class AppComponent
 	implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
@@ -19,13 +30,55 @@ export class AppComponent
 	constructor() {}
 	// Properties
 	title = 'technical';
+	buttonModel = ButtonModel;
+	overlay = false; // TODO: Move to NGRX
+	modal = false; // TODO: Move to NGRX
 	// Life cycle hooks
-	ngOnInit(): void {
-		console.log('AppComponent');
-	}
+	ngOnInit(): void {}
 	ngAfterContentInit(): void {}
 	ngAfterContentChecked(): void {}
 	ngAfterViewInit(): void {}
 	ngAfterViewChecked(): void {}
 	ngOnDestroy(): void {}
+	// Methods
+	buttonEmissionHandler() {
+		/**
+		 * Description:
+		 *
+		 * Handler for the button click event
+		 *
+		 * Work around for lack of NGRX
+		 *
+		 * // TODO: Pull this when NGRX is implemented
+		 *
+		 */
+		this.setOverlay();
+		this.setModal();
+	}
+	setModal(): void {
+		/**
+		 * Description:
+		 *
+		 * Sets modal property either displaying or hiding the modal
+		 *
+		 * Work around for lack of NGRX
+		 *
+		 * // TODO: Pull this when NGRX is implemented
+		 *
+		 */
+		this.modal = this.modal ? false : true;
+	}
+	setOverlay(): void {
+		/**
+		 * Description:
+		 *
+		 * Sets overlay property either displaying or hiding the overlay
+		 *
+		 * Work around for lack of NGRX
+		 *
+		 * // TODO: Pull this when NGRX is implemented
+		 *
+		 */
+		this.overlay = this.overlay ? false : true;
+	}
 }

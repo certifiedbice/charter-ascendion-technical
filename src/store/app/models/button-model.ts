02e9bb7073c73ca05b1373a98048097c935061ff
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-12-01 02:36:51 -0700

    feat(Architecture): Updated button models
    Added additional models

--- src/store/app/models/button-model.ts
+++ src/store/app/models/button-model.ts
@@ -10,5 +10,29 @@ const ButtonModel = {
 	name: 'modal-toggle-btn',
 	value: 'The dude abides button clicks',
 };
+const CancelButtonModel = {
+	/**
+	 * Purpose:
+	 *
+	 * Model for defining a button's parameters.
+	 */
+	type: '',
+	id: 'modal-cancel-btn',
+	classList: ['button'],
+	name: 'modal-cancel-btn',
+	value: 'Cancel',
+};
+const SubmitButtonModel = {
+	/**
+	 * Purpose:
+	 *
+	 * Model for defining a button's parameters.
+	 */
+	type: 'submit',
+	id: 'modal-submit-btn',
+	classList: ['button'],
+	name: 'modal-submit-btn',
+	value: 'Submit',
+};
 
-export default ButtonModel;
+export { ButtonModel, CancelButtonModel, SubmitButtonModel };

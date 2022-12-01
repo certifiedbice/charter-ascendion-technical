e7182d502f19ed60981fb3327f52b5893a7620f6
Bice <59291964+certifiedbice@users.noreply.github.com>
2022-12-01 02:37:32 -0700

    feat(Architecture): Updated modal model
    Added additional comments and updated body with lorem ipsum

--- src/store/app/models/modal-model.ts
+++ src/store/app/models/modal-model.ts
@@ -3,9 +3,20 @@ const ModalModel = {
 	 * Purpose:
 	 *
 	 * Model for defining an modal's parameters.
+	 *
+	 * For demonstration purposes, the actions or
+	 * buttons will be a simple array of strings but
+	 * ultimately could be considered an array of objects
+	 * for mucho configuracion.
+	 *
+	 * Another note, the "external model" file could be
+	 * included within the component itself so as to promote
+	 * a SOLID component but in my demo I have also included
+	 * the option to implement this component within an NGRX
+	 * store as well, leaving several options for implementation.
 	 */
 	actions: ['submit', 'cancel'],
-	body: 'Test Modal 01, Body text',
+	body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	classList: ['modal'],
 	id: 'modal01',
 	title: 'Test Modal 01',
